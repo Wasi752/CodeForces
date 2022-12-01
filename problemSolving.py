9f4540d7d48db7476363a26b4e22a495c4cdcf4e
https://github.com/Wasi752 <osiur2@gmail.com>
2022-12-01 20:48:51 +0600

    Next Round

--- problemSolving.py
+++ problemSolving.py
@@ -1,3 +1,11 @@
-
+person, upNum = map(int, input().split())
+eachNum = list(map(int, input().split()))
+eachNum.sort(reverse=True)
+mark = eachNum[upNum - 1]
+counter = 0
+for x in eachNum:
+    if mark <= x and x > 0:
+        counter = counter + 1
+print(counter)
 
 
